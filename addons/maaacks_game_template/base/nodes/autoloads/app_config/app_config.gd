7334cf98a39163995daa8426735d4b3005b3673d
Richard Melito <richard.melito@yourcodemayvary.blog>
2026-04-17 20:10:28 -0400

    pause menu working but have weird delay when opening the main menu

--- addons/maaacks_game_template/base/nodes/autoloads/app_config/app_config.gd
+++ addons/maaacks_game_template/base/nodes/autoloads/app_config/app_config.gd
@@ -6,5 +6,5 @@ extends Node
 @export_file("*.tscn") var ending_scene_path : String
 
 func _ready() -> void:
-	GlobalState.open()
-	AppSettings.set_from_config_and_window(get_window())
+    GlobalState.open()
+    AppSettings.set_from_config_and_window(get_window())
